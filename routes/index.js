e30a8954bb5bea5c7640b615c654bbb1aa479aea
ChessMaster <127264877+Isaquitopower@users.noreply.github.com>
2024-06-24 16:08:30 -0600

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const routes = {
+  MENU: "menu",
+
+  START: {
+    INDEX: "start",
+  },
+
+  TAB: {
+    HOME: "tabHome",
+    FAVORITES: "tabFavorites",
+    OPTIONS: "tabOptions",
+    USER: "tabUser",
+  },
+
+  HOME: {
+    INDEX: "home",
+    DETAIL: "detail",
+  },
+
+  FAVORITES: {
+    INDEX: "favorites",
+  },
+
+  OPTIONS: {
+    INDEX: "options",
+  },
+
+  USER: {
+    INDEX: "user",
+  },
+};
+
+export { routes };
